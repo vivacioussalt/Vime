b5c28e14ee750732fce69b6dea89a512f898e42c
Carl Chen <carlchen214@gmail.com>
2016-07-26 16:57:15 -0700

    Add console.logs inside functions to see what is triggered when

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Router, Route} from 'react-router';
 import App from './components/app.jsx';
-import Record from './components/RecordVime.jsx';
+import Record from './components/Record.jsx';
 import VideoPlayer from './components/VideoPlayer.jsx';
 
 export default (

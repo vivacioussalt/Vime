e1a82a9c8f5f1d8fa91ef26b0dd51051afb1d65b
Frances <franyang99@gmail.com>
2016-07-26 14:34:53 -0700

    Create users table

--- server/db/db.js
+++ server/db/db.js
@@ -9,7 +9,6 @@
 var Sequelize = require('sequelize'); 
 require('dotenv').config();
 
-
 var db = new Sequelize(process.env.DATABASE_URL, {
   protocol: 'postgres',
   dialect: 'postgres',
@@ -19,22 +18,15 @@ var db = new Sequelize(process.env.DATABASE_URL, {
   }
 });
 
-
-// TODO implement User and User Auth
 var User = db.define('user', {
   username: Sequelize.STRING, 
   password: Sequelize.STRING,
-//   partnerId: Sequelize.INTEGER   
 });
 
 var Answer = db.define('answer', {
-  //Create a unique alphanumeric id
   code: Sequelize.STRING,
   url: Sequelize.STRING
-  //Allow for users to only be viewed by intended receiver
-  // receiverId: Sequelize.INTEGER  
 });
-
 
 //Setup User Video relationship
 Answer.belongsTo(User); 
@@ -43,8 +35,6 @@ User.hasMany(Answer);
 var Question = db.define('question', {
   code: Sequelize.STRING,
   url: Sequelize.STRING
-  // Allow for certain users to receive specific questions
-  // receiverId: Sequelize.INTEGER 
 });
 
 Question.belongsTo(User);
@@ -53,12 +43,6 @@ User.hasMany(Question);
 Answer.belongsTo(Question);
 Question.hasMany(Answer);
 
-
-//Allow for users to create questions, setup relationship between user and questions
-// Question.belongsTo(User); 
-// User.hasMany(Question); 
-
-// User.sync(); 
 User.sync(); 
 Question.sync(); 
 Answer.sync();

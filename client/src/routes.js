9e86c9f9aaa0e1c73c2f7b60aa4ddeba63cbb845
Carl Chen <carlchen214@gmail.com>
2016-07-26 16:13:57 -0700

    Remove questions from Record component, add Record back to routes for testing

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {Router, Route} from 'react-router';
 import App from './components/app.jsx';
-import RecordVime from './components/RecordVime.jsx';
+import Record from './components/RecordVime.jsx';
 import VideoPlayer from './components/VideoPlayer.jsx';
 
 export default (
   <Route path="/" component={App}>
-    <Route path="record" component={RecordVime}/>
+    <Route path="record" component={Record}/>
     <Route path="videos/:id" component={VideoPlayer}/>
   </Route>
 );

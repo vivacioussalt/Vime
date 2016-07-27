a9e1bb31dc68125d549299592a7f97f6e682b555
Frances <franyang99@gmail.com>
2016-07-27 11:43:12 -0700

    (Feat) Make store available to components

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers/rootReducer';
 import {browserHistory, Router, Route} from 'react-router';
 import routes from './routes.js';
 
 ReactDOM.render(
-  <Router history={browserHistory} routes={routes}>
-  </Router>
+  <Provider store={createStore(rootReducer)}>
+    <Router history={browserHistory} routes={routes} />
+  </Provider>
   , document.getElementById('app')
 );

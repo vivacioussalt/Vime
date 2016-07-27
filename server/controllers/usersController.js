e63afefb9d242f70895299f39b883fa7f8058378
Frances <franyang99@gmail.com>
2016-07-26 19:31:17 -0700

    (Feat) Create controller for interacting with user model and test

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -1,33 +1,32 @@
-const db = require('../db/db.js');
+const User = require('../models/user');
 
-const User = db.User;
-
-function getUser(username, password) = {
+function postUser(username, password) {
   return User.findOne({ where: {username: username} })
-    .then(user => user.comparePassword(password))
+    .then(user => {
+      if (user) {
+        return getUser(user, username, password);
+      } else {
+        return createUser(user, username, password);
+      }
+    })
+    .catch(err => { throw err; });
+}
+
+function getUser(user, username, password) {
+  return user.comparePassword(password)
     .then(match => {
       if (match) {
         return user;
       } else {
-        throw new Error('Password does not match for ' + username);
+        throw new Error('Username exits or incorrect password for ' + username);
       }
     })
-    .catch(err => { console.log('Database error', err); });
 }
 
-function createUser(username, password) {
-  return User.findOne({ where: {username: username} })
-    .then(user => {
-      if (!user) {
-        return User.create({ username: username, password: password });
-      } else {
-        throw new Error('Username already exists');
-      }
-    })
-    .catch(err => { console.log('Database error', err); }) 
+function createUser(user, username, password) {
+  return User.create({ username: username, password: password });
 }
 
 module.exports = {
-  getUser: getUser,
-  createUser: createUser
+  postUser: postUser
 };
--- server/db/seed.js
+++ server/db/seed.js
@@ -1,8 +1,23 @@
-var db = require('./db.js');
 const models = require('../models/models');
-const User = models.User;
 
+const postUser = require('../controllers/userController').postUser;
+
+postUser('Gene', 'Belcher')
+.then(user => {
+  console.log('user', user);
+})
+.catch(err => { console.log(err); });
+
+
+postUser('Tina', 'Horses4Life')
+.then(user => {
+  console.log('user', user);
+})
+.catch(err => { console.log(err); });
+
+/*
 User.create({username: 'Louise', password: 'Belcher'})
   .then(() => User.findOne({username: 'Louise'}))
   .then(user => user.comparePassword('lcher'))
   .then(match => { console.log(match); })
+*/

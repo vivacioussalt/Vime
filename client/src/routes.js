b0d252315e12e0f777659adbf551ac4ae225c56b
Carl Chen <carlchen214@gmail.com>
2016-07-26 19:47:11 -0700

    Begin setting up client routes, commented out for now

--- client/src/routes.js
+++ client/src/routes.js
@@ -3,6 +3,10 @@ import {Router, Route} from 'react-router';
 import App from './components/app.jsx';
 import Record from './components/Record.jsx';
 import VideoPlayer from './components/VideoPlayer.jsx';
+// import Login from './components/Login.jsx';
+// import Question from './components/Question.jsx';
+// import Answer from './components/Answer.jsx';
+// import QandA from './components/QandA.jsx';
 
 export default (
   <Route path="/" component={App}>
@@ -10,3 +14,14 @@ export default (
     <Route path="videos/:id" component={VideoPlayer}/>
   </Route>
 );
+/*
+    <Route path="login" component={Login} />
+    <Route path="question" component={Question} />
+    <Route path="answer" component={Answer} />
+    <Route path="qa/:qaId" component={QandA} />
+*/
+// Home /
+// Login/Signup /login
+// Question /record/question
+// Answer /record/answer
+// Q&A /qa/*

eda66034515b408e558d37b198160f8734cfbaff
Frances <franyang99@gmail.com>
2016-07-26 17:33:10 -0700

    (Fix) Make comparePassword an instance method

---
+++ server/models/user.js
@@ -0,0 +1,41 @@
+const Promise = require('bluebird');
+const bcrypt = require('bcryptjs');
+
+const db = require('../db/db.js');
+
+const User = db.define('user', {
+  username: Sequelize.STRING,
+  password: Sequelize.STRING
+}, {
+  instanceMethods: {
+    comparePassword: function(password) {
+      return new Promise((resolve, reject) => {
+        bcrypt.compare(password, this.password, (err, match) => {
+          if (err) {
+            console.log('Error comparing passwords');
+            reject(err);
+          }
+          resolve(match);
+        });
+      })
+    }
+  }
+});
+
+// make sure to hash the password before creating model
+User.beforeCreate((user, options) => {
+  return new Promise((resolve, reject) => {
+    bcrypt.hash(user.dataValues.password, 10, (err, hash) => {
+      if (err) {
+        console.log('Error hashing password');
+        reject(err);
+      }
+      user.dataValues.password = hash;
+      resolve(hash);
+    });
+  })
+});
+
+User.sync();
+
+module.export = User;

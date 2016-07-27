0100e13470c9068ebf7c6be5e25da72f740c943c
1kindjune <ejw322@nyu.edu>
2016-07-26 19:50:25 -0700

    Comment out methods that are not currently in use

--- server/controllers/videosController.js
+++ server/controllers/videosController.js
@@ -1,10 +1,10 @@
 var db = require('../db/db.js');
+var shortid = require('shortid');
 var AWS = require('aws-sdk');
 require('dotenv').config();
 AWS.config.update({accessKeyId: process.env.ACCESS_KEY_ID, secretAccessKey: process.env.SECRET_ACCESS_KEY});
 //Default region
 AWS.config.update({region: 'us-east-1'});
-var shortid = require('shortid');
 
 //Create a pre-signed url which will be used by the client to post video to S3
 var generatePreSignedUrl = function(req, res) {
@@ -31,38 +31,38 @@ var generatePreSignedUrl = function(req, res) {
   res.send({preSignedUrl: preSignedUrl, publicUrl: publicUrl});  
 };
 
-//Get video by code and send video to client
-var getVideo = function(req, res) {
-  var code = req.query.code;
-  console.log('Getting video with code:', code);
-  db.Video.findOne({ 
-    where: { code: code } 
-  }).then(function(video) {
-    res.send(video);
-  });
-};
+// //Get video by code and send video to client
+// var getVideo = function(req, res) {
+//   var code = req.query.code;
+//   console.log('Getting video with code:', code);
+//   db.Video.findOne({ 
+//     where: { code: code } 
+//   }).then(function(video) {
+//     res.send(video);
+//   });
+// };
 
-//Create video with aws public url and uniq code
-//Send code to client on success
-var createVideo = function(req, res) {
-  var url = req.body.publicUrl;
-  var code = shortid.generate();
-  console.log('Creating video with url:', url);
-  db.Video.create({
-    url: url,
-    code: code
-  })
-  .then(function(video) {
-    console.log('created video:', video);
-    res.send({
-      success: 'video created',
-      code: video.code
-    });
-  });
-};
+// //Create video with aws public url and uniq code
+// //Send code to client on success
+// var createVideo = function(req, res) {
+//   var url = req.body.publicUrl;
+//   var code = shortid.generate();
+//   console.log('Creating video with url:', url);
+//   db.Video.create({
+//     url: url,
+//     code: code
+//   })
+//   .then(function(video) {
+//     console.log('created video:', video);
+//     res.send({
+//       success: 'video created',
+//       code: video.code
+//     });
+//   });
+// };
 
 module.exports = {
-  generatePreSignedUrl: generatePreSignedUrl,
-  getVideo: getVideo,
-  createVideo: createVideo
+  generatePreSignedUrl: generatePreSignedUrl
+  //getVideo: getVideo,
+  //createVideo: createVideo
 };

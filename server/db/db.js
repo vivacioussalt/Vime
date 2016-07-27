633768e5d8b1ecaafc39a52b64e534db5b948e11
Keith Wong <keith.wong3672@gmail.com>
2016-07-26 17:06:06 -0700

    Connect to Heroku Database and create tables

--- server/db/db.js
+++ server/db/db.js
@@ -7,17 +7,23 @@
 // Then click open psql in the menu bar 
 
 var Sequelize = require('sequelize'); 
-
+require('dotenv').config();
+console.log(process.env)
 //If production use production database
 if (process.env.DATABASE_URL) {
   var db = new Sequelize(process.env.DATABASE_URL, {
     protocol: 'postgres',
     dialect: 'postgres',
-    host: process.env.DATABASE_URL.split(':')[2]
+    host: process.env.DATABASE_URL.split(':')[2],
+  //     port: 5432,
+  // host: 'ec2-54-243-199-79.compute-1.amazonaws.com',
+  dialectOptions: {
+    ssl: true,
+  },
   })
 } else {
-  //Change the arguments to sequelize as neccessary ('Database', 'username', 'password')
-  var db = new Sequelize('greenfield', null, null, {
+  // Change the arguments to sequelize as neccessary ('Database', 'username', 'password')
+  var db = new Sequelize('legacy', null, null, {
     protocol: 'postgres', // or mysql
     dialect: 'postgres', // or mysql
     host: 'localhost'
@@ -26,13 +32,13 @@ if (process.env.DATABASE_URL) {
 
 
 // TODO implement User and User Auth
-// var User = db.define('user', {
-//   username: Sequelize.STRING, 
-//   password: Sequelize.STRING,
+var User = db.define('user', {
+  username: Sequelize.STRING, 
+  password: Sequelize.STRING,
 //   partnerId: Sequelize.INTEGER   
-// });
+});
 
-var Video = db.define('video', {
+var Answer = db.define('answer', {
   //Create a unique alphanumeric id
   code: Sequelize.STRING,
   url: Sequelize.STRING
@@ -42,14 +48,21 @@ var Video = db.define('video', {
 
 
 //Setup User Video relationship
-// Video.belongsTo(User); 
-// User.hasMany(Video);
+Answer.belongsTo(User); 
+User.hasMany(Answer);
 
 var Question = db.define('question', {
-  txt: Sequelize.STRING
+  code: Sequelize.STRING,
+  url: Sequelize.STRING
   // Allow for certain users to receive specific questions
   // receiverId: Sequelize.INTEGER 
 });
+
+Question.belongsTo(User);
+User.hasMany(Question);
+
+Answer.belongsTo(Question);
+Question.hasMany(Answer);
 
 
 //Allow for users to create questions, setup relationship between user and questions
@@ -57,10 +70,12 @@ var Question = db.define('question', {
 // User.hasMany(Question); 
 
 // User.sync(); 
-Video.sync(); 
+User.sync(); 
 Question.sync(); 
+Answer.sync();
 
 module.exports = {
-  Video: Video, 
-  Question: Question
+  Answer: Answer, 
+  Question: Question,
+  User: User
 }

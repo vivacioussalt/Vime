0c4615d02538524c365963a91b915e789836c912
Frances <franyang99@gmail.com>
2016-07-27 12:16:07 -0700

    (Chore) Add redux middleware

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,8 @@ import { Provider } from 'react-redux';
 import { Router, Route, browserHistory } from 'react-router';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
+import thunk from 'redux-thunk';
+import makeLogger from 'redux-logger';
 import rootReducer from './reducers/rootReducer';
 import routes from './routes.js';
 
@@ -11,7 +13,8 @@ const store = createStore(
   combineReducers({
     rootReducer,
     routing: routerReducer
-  })
+  }),
+  applyMiddleware(thunk, makeLogger())
 );
 
 const history = syncHistoryWithStore(browserHistory, store);

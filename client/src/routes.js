7434ed9fb7ea48b6c9f0b70f9bd1b56195c6b92c
Frances <franyang99@gmail.com>
2016-07-26 20:47:18 -0700

    (Fix) Edit to match new file name

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Router, Route} from 'react-router';
-import App from './components/app.jsx';
+import App from './components/App.jsx';
 import Record from './components/Record.jsx';
 import VideoPlayer from './components/VideoPlayer.jsx';
 

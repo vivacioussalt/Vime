0a0148629b061d58ed66f0a1d0b2b9041e23300f
Frances <franyang99@gmail.com>
2016-07-27 12:00:02 -0700

    (Chore) User react-router-redux to keep location state in sync

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { Router, Route, browserHistory } from 'react-router';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
 import rootReducer from './reducers/rootReducer';
-import {browserHistory, Router, Route} from 'react-router';
 import routes from './routes.js';
 
+const store = createStore(
+  combineReducers({
+    rootReducer,
+    routing: routerReducer
+  })
+);
+
+const history = syncHistoryWithStore(browserHistory, store);
+
 ReactDOM.render(
-  <Provider store={createStore(rootReducer)}>
-    <Router history={browserHistory} routes={routes} />
+  <Provider store={store}>
+    <Router history={history} routes={routes} />
   </Provider>
   , document.getElementById('app')
 );

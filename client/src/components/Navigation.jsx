263e85a625482881cb8ff6ddbd9a87b7b36d0b50
1kindjune <ejw322@nyu.edu>
2016-07-28 14:09:42 -0700

    Add in conditional react according to who is logged in

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,20 +1,51 @@
 import React from 'react'
 import { Link } from 'react-router';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import logout from '../actions/logout';
 
-const Navigation = () => (
-  <nav className="light-blue lighten-1" role="navigation">
-    <div className="nav-wrapper container">
-      <img className="logo" src="/assets/images/grandiose-potatoe.gif" height="100%"/>
-      <a id="logo-container" href="/" className="brand-logo"> Vime </a>
-      <a href="#" data-activates="mobile-demo" className="right button-collapse"><i className="material-icons">menu</i></a>
-      <ul id="nav-mobile" className="right hide-on-med-and-down">
-        <li><Link to="/login" className="btn-large waves-effect waves-light blue darken-1">Login/Signup</Link></li>
-      </ul>
-      <ul className="side-nav" id="mobile-demo">
-        <li><Link to="/login" className="btn-large waves-effect waves-light blue darken-1">Login/Signup</Link></li>
-      </ul>
-    </div>
-  </nav>
-);
+class Navigation extends React.Component {
+  handleLogout(){
+    this.props.logout();
+  }
+  render() {
+    return (
+      <nav className="light-blue lighten-1" role="navigation">
+        <div className="nav-wrapper container">
+          <img className="logo" src="/assets/images/grandiose-potatoe.gif" height="100%"/>
+          <a id="logo-container" href="/" className="brand-logo"> Vime </a>
+          <a href="#" data-activates="mobile-demo" className="right button-collapse"><i className="material-icons">menu</i></a>
+            {this.props.user ? 
+              <ul id="nav-mobile" className="right hide-on-med-and-down">
+                <li><Link to="/user" className="btn-large waves-effect waves-light blue darken-1">User Profile</Link></li> 
+                <li><button onclick={this.handleLogout}>Log Out</button></li>
+              </ul>
+              :
+              <ul id="nav-mobile" className="right hide-on-med-and-down">
+                <li><Link to="/login" className="btn-large waves-effect waves-light blue darken-1">Login/Signup</Link></li>
+              </ul>
+            }
+          <ul className="side-nav" id="mobile-demo">
+            <li><Link to="/login" className="btn-large waves-effect waves-light blue darken-1">Login/Signup</Link></li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
 
-export default Navigation;
+function mapStateToProps(state) {
+  return {
+    user: state.user,
+  };
+}
+function mapDispatchToProps(dispatch){
+  return {
+    logout: bindActionCreators(logout,dispatch)
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Navigation);
+
+// 18002492645
+// 72491920

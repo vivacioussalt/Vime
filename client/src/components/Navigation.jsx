19e9186d1a7da36d46c272430521fbc21b9bfeff
1kindjune <ejw322@nyu.edu>
2016-07-28 14:45:28 -0700

    Minor edit with location

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -19,10 +19,13 @@ class Navigation extends React.Component {
           <img className="logo" src="/assets/images/grandiose-potatoe.gif" height="100%"/>
           <a id="logo-container" href="/" className="brand-logo"> Vime </a>
           <a href="#" data-activates="mobile-demo" className="right button-collapse"><i className="material-icons">menu</i></a>
-            {this.props.user ? 
+            {this.props.location === '/login' ? 
+            '' 
+            :
+            this.props.user ? 
               <ul id="nav-mobile" className="right hide-on-med-and-down">
-                <li><Link to="/user" className="btn-large waves-effect waves-light blue darken-1">User Profile</Link></li> 
                 <li><button onClick={this.handleLogout}>Log Out</button></li>
+                <li><Link to="/profile" className="btn-large waves-effect waves-light blue darken-1">User Profile</Link></li> 
               </ul>
               :
               <ul id="nav-mobile" className="right hide-on-med-and-down">
@@ -41,6 +44,8 @@ class Navigation extends React.Component {
 function mapStateToProps(state) {
   return {
     user: state.user,
+    //loc: ownProps,
+    location: state.routing.locationBeforeTransitions.pathname
   }
 }
 function mapDispatchToProps(dispatch){

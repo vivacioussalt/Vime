3eca3e60b393747b013045ebe67f330416669948
Carl Chen <carlchen214@gmail.com>
2016-07-27 20:11:43 -0700

    add answersOfQuestions reducer

---
+++ client/src/reducers/answersOfQuestions.js
@@ -0,0 +1,17 @@
+export default function answersOfQuestions(state = {}, action) {
+  switch (action.type) {
+    case 'ADD_ANSWER':
+      return {
+        ...state,
+        state[action.questionId]: state[action.questionId].concat(action.answer)
+      }
+    case 'GET_ANSWERS':
+      return {
+        ...state,
+        state[action.questionId]: action.answers
+        // action.answers must be formatted properly as an array of objects {answer.id: answer } 
+      }
+    default:
+      return state;
+  }
+}

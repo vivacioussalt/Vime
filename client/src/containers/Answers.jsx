43246bce54b54c353bb6831f19ebcb22b8f2c755
Carl Chen <carlchen214@gmail.com>
2016-07-27 20:11:14 -0700

    Create Answers container for videoplayer

---
+++ client/src/containers/Answers.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class Answers extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    <div> 
+      <VideoPlayer />
+    </div>
+  }
+}
+

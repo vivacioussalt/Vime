cbcfae608e186b716ed7043dbe757c0020fbd490
1kindjune <ejw322@nyu.edu>
2016-07-28 10:20:23 -0700

    Remove unused video player from routes

--- client/src/routes.js
+++ client/src/routes.js
@@ -3,7 +3,6 @@ import {Router, Route} from 'react-router';
 import App from './components/App.jsx';
 import Login from './containers/Login.jsx';
 import RecordQuestion from './containers/RecordQuestion.jsx';
-import VideoPlayer from './components/VideoPlayer.jsx';
 import Error from './components/Error.jsx';
 
 export default (

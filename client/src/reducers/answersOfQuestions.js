2b9853731b9357b5e698748d013f775260c1070b
Carl Chen <carlchen214@gmail.com>
2016-07-28 11:24:17 -0700

    set up RecordAnswer container to redirect to Topic container. BUG: ADD_ANSWER action is broken for questionId

--- client/src/reducers/answersOfQuestions.js
+++ client/src/reducers/answersOfQuestions.js
@@ -1,14 +1,21 @@
 export default function answersOfQuestions(state = {}, action) {
   switch (action.type) {
     case 'ADD_ANSWER':
+    if (state[action.questionCode]) {
       return {
         ...state,
-        state[action.questionCode]: state[action.questionCode].concat(action.answer)
+        [action.questionCode]: state[action.questionCode].concat(action.answer)
+      }      
+    } else {
+      return {
+        ...state,
+        [action.questionCode]: [action.answer]
       }
+    }
     case 'ADD_ALL_ANSWERS':
       return {
         ...state,
-        state[action.questionCode]: action.answers
+        [action.questionCode]: action.answers
         // action.answers must be formatted properly as an array of objects {answer.id: answer } 
       }
     default:

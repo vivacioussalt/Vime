c8e287a365b06ec774bad87a1209dcc4c18d0b2d
Frances <franyang99@gmail.com>
2016-07-27 19:39:29 -0700

    (Chore) Add fetch polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   entry: [
-    __dirname + '/client/src/index.js'
+    __dirname + '/client/src/index.js',
+    'whatwg-fetch'
   ],
   output: {
     path: __dirname + '/client',

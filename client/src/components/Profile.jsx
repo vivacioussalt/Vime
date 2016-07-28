f596ec4ed574fdd6966b944de98fea82f89a40c1
Frances <franyang99@gmail.com>
2016-07-28 16:05:09 -0700

    (Feat) Create user profile page

---
+++ client/src/components/Profile.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import QuestionVideoGrid from './QuestionVideoGrid';
+import AnswerVideoGrid from './AnswerVideoGrid';
+
+const Profile = ({ user, questions, answers }) => (
+  <div>
+    <div>
+      <h2>{user.username}</h2>
+    </div>
+    <div className="container">
+      <h3>My Questions</h3>
+      <QuestionVideoGrid videos={questions} />
+    </div>
+    <div className="container">
+      <h3>My Answers</h3>
+      <AnswerVideoGrid videos={answers} />
+    </div>
+  </div>
+);
+
+export default Profile;

3d588a319aa63e378de2378b30cbff8adeb92dad
Frances <franyang99@gmail.com>
2016-07-27 21:03:11 -0700

    (Refactor) Use findOrCreate to shorten code

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -1,7 +1,7 @@
 const User = require('../models/user');
 
 const existsMessage = 'Username exists or incorrect password';
-
+/*
 function postUser(req, res) {
   const body = req.body;
   findOrCreate(body.username, body.password)
@@ -15,6 +15,32 @@ function postUser(req, res) {
       res.sendStatus(500);
     }
   });
+}
+*/
+function postUser(req, res) {
+  const body = req.body;
+  User.findOrCreate({
+    where: {username: body.username},
+    defaults: {password: body.password}
+  })
+  .spread((user, created) => {
+    if (created) {
+      res.json(user.dataValues); 
+    } else {
+      user.comparePassword(body.password)
+      .then(match => {
+        if (match) {
+          res.json(user.dataValues);
+        } else {
+          res.json({message: 'Username exists/Incorrect Password'});
+        }
+      })
+    }
+  })
+  .catch(err => {
+    console.log('Post user error', err);
+    res.sendStatus(500);
+  })
 }
 
 function findOrCreate(username, password) { 

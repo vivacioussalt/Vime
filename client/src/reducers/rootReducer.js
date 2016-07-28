eee7f6004e0cf01478f51cee28640090cdcd5435
Frances <franyang99@gmail.com>
2016-07-27 19:45:19 -0700

    (Feat) Add user reducer to root reducer

--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -1,11 +1,12 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
-
+import user from './user';
 import questionsById from './questionsById';
 //import answersOfQuestion from './answersOfQuestion';
 
 const rootReducer = combineReducers({
   questionsById,
+  user,
   routing: routerReducer
 });
 

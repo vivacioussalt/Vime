beb4b24c1177fb615219b0c08921d94420cb1254
Carl Chen <carlchen214@gmail.com>
2016-07-27 21:38:20 -0700

    map actions to RecordAnswer container

--- client/src/containers/RecordAnswer.jsx
+++ client/src/containers/RecordAnswer.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import Record from './../components/Record.jsx';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-// import actions
+
+import answerActions from './../actions/answerActions.jsx';
+
 
 class RecordAnswer extends React.Component {
   constructor(props) {
@@ -11,8 +13,16 @@ class RecordAnswer extends React.Component {
 
   render() {
     <div>
-      <Record />
+      <Record addToState={this.props.actions.addAnswer} apiUrl={'/api/answers'}/>
     </div>
   }
 }
 
+
+function mapDispatchToProps(dispatch){
+  return {
+    actions: bindActionCreators(answerActions, dispatch)
+  }
+}
+
+export default connect(null, mapDispatchToProps)(RecordAnswer);

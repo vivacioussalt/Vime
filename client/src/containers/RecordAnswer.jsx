e76b36be5334cc073701ac1b040c7a489e7b3a61
Carl Chen <carlchen214@gmail.com>
2016-07-27 20:11:32 -0700

    add RecordAnswer container

---
+++ client/src/containers/RecordAnswer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Record from './../components/Record.jsx';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+// import actions
+
+class RecordAnswer extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    <div>
+      <Record />
+    </div>
+  }
+}
+

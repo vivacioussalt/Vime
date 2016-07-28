8a6f93e45d5b03d4a0f7e507896effd6495f31ce
Carl Chen <carlchen214@gmail.com>
2016-07-27 20:14:53 -0700

    add answersOfQuestions reducer to rootReducer

--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -2,11 +2,12 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import user from './user';
 import questionsById from './questionsById';
-//import answersOfQuestion from './answersOfQuestion';
+import answersOfQuestion from './answersOfQuestion';
 
 const rootReducer = combineReducers({
   questionsById,
   user,
+  answersOfQuestion,
   routing: routerReducer
 });
 

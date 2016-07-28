9da0e9e5104a94f11df80d1c668fca41727fbc74
Frances <franyang99@gmail.com>
2016-07-27 19:35:29 -0700

    (Feat) Create login action

--- client/src/actions/login.jsx
+++ client/src/actions/login.jsx
@@ -1,7 +1,24 @@
-export default function login(username, password) {
+function setUser(user) {
   return {
-    type: 'LOGIN',
-    username,
-    password
-  } 
+    type: 'LOGIN_SUCCESS',
+    user
+  };
+}
+
+export default function login(username, password) {
+  return dispatch => {
+    return fetch('http://localhost:3000/api/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        username: username,
+        password: password
+      })
+    })
+    .then(res => res.json())
+    .then(user => {console.log(user); return dispatch(setUser(user));})
+    .catch(err => {
+      console.log(err);
+    })
+  }
 }

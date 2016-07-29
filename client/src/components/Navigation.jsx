84cc0d78f530c5b4fa4f0e84b3878500ab0fc156
1kindjune <ejw322@nyu.edu>
2016-07-28 23:01:55 -0700

    Set nav bar without hamburgers

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -16,25 +16,35 @@ class Navigation extends React.Component {
     return (
       <nav className="light-blue lighten-1" role="navigation">
         <div className="nav-wrapper container">
-          <img className="logo" src="/assets/images/grandiose-potatoe.gif" height="100%"/>
+          <img className="logo hide-on-med-and-down" src="/assets/images/grandiose-potatoe.gif" height="100%"/>
           <a id="logo-container" href="/" className="brand-logo"> Vime </a>
-          <a href="#" data-activates="mobile-demo" className="right button-collapse"><i className="material-icons">menu</i></a>
-            {this.props.location === '/login' ? 
-            '' 
+          {this.props.location === '/login' ? 
+          '' 
+          :
+          this.props.user ? 
+            <ul id="nav-mobile" className="right hide-on-med-and-down">
+              <li><button onClick={this.handleLogout} className="btn-flat btn-large waves-effect waves-light blue darken-1">Log Out</button></li>
+              <li><Link to="/profile" className="btn-flat btn-large waves-effect waves-light blue darken-1"><i className="material-icons">perm_identity</i></Link></li> 
+            </ul>
             :
-            this.props.user ? 
-              <ul id="nav-mobile" className="right hide-on-med-and-down">
-                <li><button onClick={this.handleLogout}>Log Out</button></li>
-                <li><Link to="/profile" className="btn-large waves-effect waves-light blue darken-1">User Profile</Link></li> 
-              </ul>
-              :
-              <ul id="nav-mobile" className="right hide-on-med-and-down">
-                <li><Link to="/login" className="btn-large waves-effect waves-light blue darken-1">Login/Signup</Link></li>
-              </ul>
-            }
-          <ul className="side-nav" id="mobile-demo">
-            <li><Link to="/login" className="btn-large waves-effect waves-light blue darken-1">Login/Signup</Link></li>
-          </ul>
+            <ul id="nav-mobile" className="right hide-on-med-and-down">
+              <li><Link to="/login" className="btn-flat btn-large waves-effect waves-light blue darken-1">Login/Signup</Link></li>
+            </ul>
+          }  
+
+          {this.props.location === '/login' ? 
+          '' 
+          :
+          this.props.user ? 
+            <div id="nav-mobile">
+              <button onClick={this.handleLogout} className="btn-flat btn waves-effect waves-light blue darken-1 hide-on-large-only" style={{'float':'left', 'height':'100%', 'width':'30%'}}>Logout</button>
+              <Link to="/profile" className="btn-flat btn-large waves-effect waves-light blue darken-1 hide-on-large-only" style={{'float':'right', 'height':'100%','width':'30%'}}><i className="material-icons">perm_identity</i></Link>
+            </div>               
+            :
+            <ul id="nav-mobile" className="right show-on-medium-and-down hide-on-med-and-up">
+              <li><Link to="/login" className="btn-flat btn-small waves-effect waves-light blue darken-1">Login/Signup</Link></li>
+            </ul>
+          }  
         </div>
       </nav>
     );

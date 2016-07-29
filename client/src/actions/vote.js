19d8c435e462f10775e0b497bdf698f0d3224de0
Frances <franyang99@gmail.com>
2016-07-29 16:04:40 -0700

    (Feat) Dispatch action to make put request to server updating a question's votes

--- client/src/actions/vote.js
+++ client/src/actions/vote.js
@@ -1,17 +1,22 @@
+import { checkStatus, toJSON } from './fetchUtils';
 import { addQuestion } from './questionAction';
 
-function vote(prevValue, type, videoType, id) {
+function vote(type, videoType, video) {
+  const value = type === 'upvote' ? video.upvote + 1 : video.downvote + 1;
   return dispatch => {
     fetch(`http://localhost:3000/api/${videoType}`, {
       method: 'PUT',
       headers: { 'Content-Type': 'application/json' }, 
-      body: {
-        id,
+      body: JSON.stringify({
+        id: video.id,
         type,
-        value: prevValue + 1,
-      }
-    }
-    .then(question => dispatch(addQuestion(question)));
+        value: value,
+      })
+    })
+    .then(checkStatus)
+    .then(toJSON)
+    .then(question => dispatch(addQuestion(question)))
+    .catch(err => { console.log(err); })
   }
 }
 

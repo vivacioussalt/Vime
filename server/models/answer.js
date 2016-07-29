d167d12b3f4ac764a705a420f59a6fe00168aa9f
Frances <franyang99@gmail.com>
2016-07-28 21:59:32 -0700

    (Fix) typo for models

--- server/models/answer.js
+++ server/models/answer.js
@@ -4,6 +4,6 @@ const db = require('../db/db.js');
 module.exports = db.define('answer', {
   code: Sequelize.STRING,
   url: Sequelize.STRING,
-  upvote: { data: Sequelize.INTEGER, defaultValue: 0 },
-  downvote: { data: Sequelize.INTEGER, defaultValue: 0 }
+  upvote: { type: Sequelize.INTEGER, defaultValue: 0 },
+  downvote: { type: Sequelize.INTEGER, defaultValue: 0 }
 });

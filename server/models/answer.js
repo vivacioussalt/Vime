1474a5634d2cadb1e2808da7b08b07c2d6ab6a9b
Frances <franyang99@gmail.com>
2016-07-28 21:51:53 -0700

    (Fix) Give default value of 0 to upvote and downvote

--- server/models/answer.js
+++ server/models/answer.js
@@ -4,6 +4,6 @@ const db = require('../db/db.js');
 module.exports = db.define('answer', {
   code: Sequelize.STRING,
   url: Sequelize.STRING,
-  upvote: Sequelize.INTEGER,
-  downvote: Sequelize.INTEGER
+  upvote: { data: Sequelize.INTEGER, defaultValue: 0 },
+  downvote: { data: Sequelize.INTEGER, defaultValue: 0 }
 });

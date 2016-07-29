3e00d28484be9ed4e856a1cf85cf8b52085713bb
Carl Chen <carlchen214@gmail.com>
2016-07-28 21:21:16 -0700

    fetch answers for updating state for Topic container when clicking on a Question video in Home container

--- client/src/actions/answerActions.jsx
+++ client/src/actions/answerActions.jsx
@@ -18,49 +18,26 @@ function addAnswer({questionCode, answer}) {
   }
 }
 
-function addAllAnswers(answers) {
+function addAllAnswers(code, answers) {
   return {
     type: 'ADD_ALL_ANSWERS',
+    questionCode: code,
     answers
   }
 }
 
-// export function getQuestions() {
-//   return function(dispatch) {
-//     return fetch('http://localhost:3000/api/questions')
-//       .then(checkStatus)
-//       .then(toJSON)
-//       .then(questions => {
-//         dispatch(addAllQuestions(questions))
-//       })
-//       .catch(err => console.warn('Error in getQuestions', err))
-//   }
-// }
-
-// fetch('/api/answers', {
-//   method: 'GET',
-//   headers: {
-//     'Accept': 'application/json',
-//     'Content-Type': 'application/json'
-//   },
-//   body: JSON.stringify({
-//     code: ,
-//   })
-// })
-
-function getAnswers() {
+function getAnswersForQuestion(id, code) {
   return function(dispatch) {
-    return fetch('http://localhost/api/answers')
-      .then(res => {
-        if (!res.ok) {
-          console.warn('error in getAnswers fetch', res.statusText);
-        }
-        var answers = res.json()
-        return answers;
-      })
-      .then(answers => dispatch(addAllAnswers(answers)))
-      .catch(err => console.warn('error in getAnswers', err))
+    return fetch(`/api/answers?questionId=${id}`)
+    .then(checkStatus)
+    .then(toJSON)
+    .then(answers => {
+      console.log(answers);
+      dispatch(addAllAnswers(code, answers));
+      dispatch(push(`/qa/${code}`));
+    })
+    .catch(err => console.warn('error in getAnswers', err))
   }
 }
 
-export { addAnswer, addAllAnswers, getAnswers };
+export { addAnswer, addAllAnswers, getAnswersForQuestion };

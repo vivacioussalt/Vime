38ac04a3fbaf20c9e6991db3ea3ce1f1f5082834
Frances <franyang99@gmail.com>
2016-07-30 11:54:29 -0700

    (Cleanup) Make router deal with db accessing

--- server/server.js
+++ server/server.js
@@ -3,13 +3,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const router = require('./routes');
 
-const app = express();
-app.port = process.env.PORT || 3000;
-const server = app.listen(app.port, function() {
-  console.log('we are listening!');
-});
+const stripeHandler = require('./utility/stripeHandler');
 
-//app.io = require('socket.io')(server);
+const app = express();
+
 const io = require('socket.io')(server);
 
 app.use(express.static(__dirname + '/../client/public'));
@@ -17,10 +14,23 @@ app.use(express.static(__dirname + '/../client/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//middleware for routes
-//https://expressjs.com/en/guide/routing.html
-app.use('/', router);
+// use router for accessing db 
+// https://expressjs.com/en/guide/routing.html
+app.use('/api', router);
 
+// route for stripe processing
+app.post('/stripe', stripeHandler.processDonation);
+app.get('/stripe/callback', stripeHandler.getStripeId);
+
+// serve index.html for rest
+router.get('*', (req, res)=>{
+  res.sendFile(path.resolve(__dirname + '/../client/public/index.html'));
+});
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, function() {
+  console.log('we are listening!');
+});
 
 io.on('connection', function(socket){
   console.log('\nUSER CONNECTED\n');

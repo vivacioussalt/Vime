8ed5c513eb375bca6942db43ac228b32d485f669
Frances <franyang99@gmail.com>
2016-07-29 20:42:18 -0700

    (Refactor) Accept videos as array

--- client/src/components/QuestionVideoGrid.jsx
+++ client/src/components/QuestionVideoGrid.jsx
@@ -3,16 +3,16 @@ import { Link } from 'react-router';
 let QuestionVideoGrid = ({videos, fetchAnswers}) => {
   return (
     <div className="row center">
-      {Object.keys(videos).map(videoCode => 
-        <div key={videos[videoCode].id} className="col s4">
-            <video onClick={() => {fetchAnswers(videos[videoCode].id, videoCode)}} controls src={videos[videoCode].url} width="100%"/>
+      {videos.map(video => 
+        <div key={video.id} className="col s4">
+            <video onClick={fetchAnswers.bind(null, video.id, video.code)} controls src={video.url} width="100%"/>
         </div>
       )}
     </div>
   );
 };
 
-QuestionVideoGrid.propTypes = { videos: React.PropTypes.object.isRequired };
+QuestionVideoGrid.propTypes = { videos: React.PropTypes.array.isRequired };
 
 export default QuestionVideoGrid;
 

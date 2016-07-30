fb0015d70ef1aaa442f6613382f20166acf8a412
Keith Wong <keith.wong3672@gmail.com>
2016-07-29 22:44:59 -0700

    Change Stripe handler to handle custum forms and custom donation amount

--- server/utility/stripeHandler.js
+++ server/utility/stripeHandler.js
@@ -1,45 +1,37 @@
+const { User } = require('../models/models');
 const request = require('request');
 require('dotenv').config();
-var stripe = require('stripe')(process.env.STRIPE_API_KEY); // api secret key
+var stripe = require('stripe')(process.env.STRIPE_API_KEY);
 
 var processDonation = function(req, res) {
-  stripe.charges.create({
-    amount: 800, // amount in cents
-    currency: 'usd',
-    source: req.body.stripeToken,
-    destination: 'acct_18cMqjHSob4JtQzi' //stripe user id to get donations
-  }, function(err, charge) {
-    if (err && err.type === 'StripeCardError') {
-    // The card has been declined
-    // redirect user with error message
-      console.log('Donation has been declined');
-      res.send(req.body);
-    } else {
-    // redirect user letting them know successful
-      console.log('Donation successful');
-      res.send(req.body);
-    } 
+  User.findOne({ where: { id: req.body.userId } })
+  .then(function(user) {
+    stripe.charges.create({
+      amount: req.body.amount,
+      currency: 'usd',
+      source: req.body.stripeToken,
+      destination: user.dataValues.stripeId
+      // 'acct_18cMqjHSob4JtQzi' //stripe user id to get donations
+    }, function(err, charge) {
+      if (err && err.type === 'StripeCardError') {
+        console.log('Donation has been declined');
+        res.send(req.body);
+      } else {
+        console.log(req.body);
+        console.log('Donation successful');
+        res.send(req.body);
+      } 
+    });    
   });
 };
 
 var getStripeId = function(req, res) {
-  console.log('inside stripe callback handler');
-  var code = req.query.code;
 
-  request.post({
-    url: 'https://connect.stripe.com/oauth/token',
-    form: {
-      grant_type: 'authorization_code',
-      client_id: process.env.STRIPE_CLIENT_ID, // stripe client id
-      code: code,
-      client_secret: process.env.STRIPE_API_KEY // stripe secret api_key
-    }
-  }, function(err, response, body) {
-
-    // we will want to save the user's stripe id 
-    console.log(JSON.parse(body).stripe_user_id); // save this to the user database as stripe id
-    res.send(JSON.parse(body));
-    // redirect user to another page after we get their id
+  User.findOne({ where: { id: req.body.userId } }).then(function(user) {
+    user.updateAttributes({ stripeId: req.body.stripeId });
+  })
+  .then(function(updatedUser) {
+    console.log(updatedUser);
   });
 };
 

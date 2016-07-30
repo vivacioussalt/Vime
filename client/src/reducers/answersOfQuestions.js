10efcbceb9806bd223349affdebe16d06b2fdb23
Frances <franyang99@gmail.com>
2016-07-29 18:21:55 -0700

    (Refactor) Use function composition and work for rewrites

--- client/src/reducers/answersOfQuestions.js
+++ client/src/reducers/answersOfQuestions.js
@@ -1,6 +1,18 @@
+function answer(state = [], action) {
+  const index = findIndex(state, item => item.code === action.answer.code);
+  if (index === -1) {
+    return state.concat([action.answer]);
+  } else {
+    return [...state.slice(0, index), ...state.slice(index + 1), action.answer];
+  } 
+}
+
 export default function answersOfQuestions(state = {}, action) {
   switch (action.type) {
     case 'ADD_ANSWER':
+      var answers = state[action.questionCode] || [];
+      return {...state, [action.questionCode]: answer(answers, action)};
+/*
     if (state[action.questionCode]) {
       return {
         ...state,
@@ -12,6 +24,7 @@ export default function answersOfQuestions(state = {}, action) {
         [action.questionCode]: [action.answer]
       }
     }
+*/
     case 'ADD_ALL_ANSWERS':
       return {
         ...state,
@@ -21,4 +34,13 @@ export default function answersOfQuestions(state = {}, action) {
     default:
       return state;
   }
+}
+
+function findIndex(array, predicate) {
+  for (var i = 0; i < array.length; i++) {
+    if (predicate(array[i])) {
+      return i;
+    }
+  }
+  return -1;
 }

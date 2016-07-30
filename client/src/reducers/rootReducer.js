ee87c781ca8aa6a94aa74271dc53945d06bd99e9
Frances <franyang99@gmail.com>
2016-07-29 23:53:43 -0700

    (Feat) Create action/reducer for filter for questions

--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -3,11 +3,13 @@ import { routerReducer } from 'react-router-redux';
 import user from './user';
 import questionsByCode from './questionsByCode';
 import answersOfQuestions from './answersOfQuestions';
+import filter from './filter';
 
 const rootReducer = combineReducers({
   questionsByCode,
   user,
   answersOfQuestions,
+  filter,
   routing: routerReducer
 });
 

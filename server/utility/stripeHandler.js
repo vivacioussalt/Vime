85175c898accd2a20ed7519c91c33aec98e2f415
Keith Wong <keith.wong3672@gmail.com>
2016-07-30 12:46:05 -0700

    Fix bug to lookup payment by answer

--- server/utility/stripeHandler.js
+++ server/utility/stripeHandler.js
@@ -1,26 +1,30 @@
-const { User } = require('../models/models');
+const Answer = require('../models/models.js').Answer;
+const User = require('../models/models.js').User;
 const request = require('request');
 require('dotenv').config();
 var stripe = require('stripe')(process.env.STRIPE_API_KEY);
 
 var processDonation = function(req, res) {
-  User.findOne({ where: { id: req.body.userId } })
-  .then(function(user) {
-    stripe.charges.create({
-      amount: req.body.amount,
-      currency: 'usd',
-      source: req.body.stripeToken,
-      destination: user.dataValues.stripeId
-      // 'acct_18cMqjHSob4JtQzi' //stripe user id to get donations
-    }, function(err, charge) {
-      if (err && err.type === 'StripeCardError') {
-        console.log('Donation has been declined');
-        res.send(req.body);
-      } else {
-        console.log(req.body);
-        console.log('Donation successful');
-        res.send(req.body);
-      } 
+  Answer.findOne({ where: { id: req.body.videoId } })
+  .then(function(answer) {
+    User.findOne({ where: { id: answer.dataValues.UserId } })
+    .then(function(user) {
+      stripe.charges.create({
+        amount: req.body.amount,
+        currency: 'usd',
+        source: req.body.stripeToken,
+        destination: user.dataValues.stripeId
+        // 'acct_18cMqjHSob4JtQzi' //stripe user id to get donations
+      }, function(err, charge) {
+        if (err && err.type === 'StripeCardError') {
+          console.log('Donation has been declined');
+          res.send(req.body);
+        } else {
+          console.log(req.body);
+          console.log('Donation successful');
+          res.send(req.body);
+        }
+      }); 
     });    
   });
 };

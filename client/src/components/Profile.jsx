de582d704c1a0e4f8b7cfb945e4ef2c0c264b8d6
Frances <franyang99@gmail.com>
2016-07-29 21:24:13 -0700

    (Fix) Pass click handler for question vids

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,14 +1,14 @@
 import QuestionVideoGrid from './QuestionVideoGrid';
 import AnswerVideoGrid from './AnswerVideoGrid';
 
-const Profile = ({ user, questions, answers }) => (
+const Profile = ({ user, questions, answers, fetchAnswers }) => (
   <div>
     <div>
       <h2>{user.username}</h2>
     </div>
     <div className="container">
       <h3>My Questions</h3>
-      <QuestionVideoGrid videos={questions} />
+      <QuestionVideoGrid videos={questions} fetchAnswers={fetchAnswers} />
     </div>
     <div className="container">
       <h3>My Answers</h3>
